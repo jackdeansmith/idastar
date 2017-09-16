12d6e7de841f5f741e4d4113eb923c621fea856f
Jack D Smith <jack@jackdeansmith.com>
2017-09-16 13:43:41 -0400

    Nailed down the first try for the traits

--- src/idastar.rs
+++ src/idastar.rs
@@ -7,9 +7,10 @@ pub trait Solvable{
 
 //A struct represents an element in a searchable state space if we can
 //get an iterator to its children states.
-pub trait Searchable{
-    type Move;
-    fn successors(&self) -> Iterator<Item=(Self::Move, Self)>;
+pub trait Searchable: Sized{
+    type Transition: Sized;
+    type SuccIter: Iterator<Item=(Self, Self::Transition)>;
+    fn successors(&self) -> Self::SuccIter; 
 }
 
 //A struct is an element of an admissable state space if there exists an
@@ -19,7 +20,17 @@ pub trait Admissable{
 }
 
 fn search<T:Solvable + Searchable + Admissable>(state: T, cost_limit: u64) 
--> Option<Vec<T::Move>>{
+-> Option<Vec<T::Transition>>{
+
+    //The base case, if the cost limit is 0, we can't do anyting
+    if cost_limit == 0{
+        return None;
+    }
+
+    //Otherwise, we need to look recursively into the successors
+    for (next_state, next_transition) in state.successors(){
+
+    }
 
     None
 }
